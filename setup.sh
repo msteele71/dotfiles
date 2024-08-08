da26df2ee118be9952ae070e8c4c2f288680c51c
Michael Steele <m_steele71@yahoo.com>
2024-08-08 17:22:27 +0000

    adding chezmoi init for the coder dotfiles

--- setup.sh
+++ setup.sh
@@ -1,5 +1,27 @@
 #! /usr/bin/env bash
 
+CODER_DOTFILES=$HOME/.config/coderv2/dotfiles
+CHEZMOI_CONFIG=$HOME/.config/chezmoi/chezmoi.json
+
+# first configure chezmoi to use the coder dotfiles src directory
 mkdir -p ~/.config/chezmoi
-jq -n --arg source "~/.config/coderv2/dotfiles" '{"sourceDir": $source}' > ~/.config/chezmoi/chezmoi.json
-chezmoi apply
+
+#create default config if non exists
+if [ ! -f "$CHEZMOI_CONFIG" ]; then
+    jq -n '{}' > $CHEZMOI_CONFIG
+fi
+# set the sourceDir value
+echo "Setting sourceDir value for chezmoi config: $CODER_DOTFILES"
+jq --arg source $CODER_DOTFILES \
+    '.sourceDir = $source' \
+    $CHEZMOI_CONFIG > /tmp/chezmoi.config && mv /tmp/chezmoi.config $CHEZMOI_CONFIG
+
+# next link the metadata.json so that chezmoi can read it
+if [ -f "$HOME/.vworkstation/metadata.json" ]; then
+    # ensure the data dir exists
+    mkdir -p $CODER_DOTFILES/.chezmoidata
+    ln -s $HOME/.vworkstation//metadata.json $CODER_DOTFILES/.chezmoidata/vworkstation.json
+fi
+
+echo "Applying dofiles with chezmoi"
+chezmoi apply -v
