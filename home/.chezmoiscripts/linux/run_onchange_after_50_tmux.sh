19245551e6218e703695275cff17ae9983d61dcb
Michael Steele <michael.steele@deepgram.com>
2025-11-19 12:22:57 -0600

    adding existence checks and handle appropriately

--- home/.chezmoiscripts/linux/run_onchange_after_50_tmux.sh
+++ home/.chezmoiscripts/linux/run_onchange_after_50_tmux.sh
@@ -1,15 +1,33 @@
 #!/usr/bin/env bash
 
-set -e
-
 # install tmux plugins
 XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
 
 TMUX_PLUGIN_DIR=$XDG_CONFIG_HOME/tmux/plugins
 
 mkdir -p $TMUX_PLUGIN_DIR
-git clone https://github.com/tmux-plugins/tmux-resurrect $TMUX_PLUGIN_DIR/tmux-resurrect
-git clone https://github.com/tmux-plugins/tmux-continuum $TMUX_PLUGIN_DIR/tmux-continuum
-git clone -b v2.1.1 https://github.com/catppuccin/tmux.git $TMUX_PLUGIN_DIR/catppuccin
+
+# tmux-resurrect
+if [ -d "$TMUX_PLUGIN_DIR/tmux-resurrect" ]; then
+  echo "updating tmux plugin tmux-resurrect"
+  cd $TMUX_PLUGIN_DIR/tmux-resurrect && git pull
+else
+  git clone https://github.com/tmux-plugins/tmux-resurrect $TMUX_PLUGIN_DIR/tmux-resurrect
+fi
+
+# tmux-continuum
+if [ -d "$TMUX_PLUGIN_DIR/tmux-continuum" ]; then
+  echo "updating tmux plugin tmux-continuum"
+  cd $TMUX_PLUGIN_DIR/tmux-continuum && git pull
+else
+  git clone https://github.com/tmux-plugins/tmux-continuum $TMUX_PLUGIN_DIR/tmux-continuum
+fi
+
+# catppuccin
+if [ -d "$TMUX_PLUGIN_DIR/catppuccin" ]; then
+  echo "tmux plugin catpuccin (branch v2.1.1) already cloned"
+else
+  git clone -b v2.1.1 https://github.com/catppuccin/tmux.git $TMUX_PLUGIN_DIR/catppuccin
+fi
 
 echo "tmux plugins installed"
--- setup.sh
+++ setup.sh
@@ -1,11 +1,28 @@
 #! /usr/bin/env bash
 
 ##
-# This setup file serves two main purposes
-# 1. Perform any FTR setup to prepare for actually applying dotfiles
-# 2. Applying/Running the dotfiles setup, in this case using chezmoi
+# This setup file acts as a dotfile manager proxy for the coder dotfiles process.
+# Why? The coder dotfiles command clones the repo and then either executes
+# a script OR will copy everything into the $HOME directory.  NOT BOTH!
+# So if you want to use a dotfile manager then you need to have a setup
+# script handle calling the dotfile manager.
+#
+# The script below sets up chezmoi as the dotfile manager.  This script will
+# proxy the call from coder for the setup script, delegating setup to chezmoi.
 ##
 
+# this is where the coder dotfiles command puts the dotfiles
+CODER_DOTFILES=$HOME/.config/coderv2/dotfiles
+
+# check if the dotfiles directory exists
+if [ ! -d "$CODER_DOTFILES" ]; then
+    echo "WARNING: The CODER_DOTFILES directory does not exist: $CODER_DOTFILES"
+    echo "This script should only be used if the dotfiles are setup with _coder dotfiles_"
+    echo "Otherwise, use your normal dotfile manager to apply changes."
+    exit 1
+fi
+
+# this is the configuration file that chezmoi will look for
 CHEZMOI_CONFIG=$HOME/.config/chezmoi/chezmoi.json
 
 # first configure chezmoi to use the coder dotfiles src directory
@@ -16,20 +33,21 @@ if [ ! -f "$CHEZMOI_CONFIG" ]; then
     jq -n '{}' > $CHEZMOI_CONFIG
 fi
 
-# set the sourceDir value
-# echo "Setting sourceDir value for chezmoi config: $CODER_DOTFILES"
-# jq --arg source $CODER_DOTFILES \
-#     '.sourceDir = $source' \
-#     $CHEZMOI_CONFIG > /tmp/chezmoi.config && mv /tmp/chezmoi.config $CHEZMOI_CONFIG
+# set the sourceDir value so that chezmoi will find the coder specific
+# location where the dotfiles are cloned
+echo "Setting sourceDir value for chezmoi config: $CODER_DOTFILES"
+    jq --arg source $CODER_DOTFILES \
+      '.sourceDir = $source' \
+      $CHEZMOI_CONFIG > /tmp/chezmoi.config && mv /tmp/chezmoi.config $CHEZMOI_CONFIG
 
-# # next link the metadata.json so that chezmoi can read it
-# if [ -f "$HOME/.vworkstation/metadata.json" ] && [ ! -f "$CODER_DOTFILES/.chezmoidata/vworkstation.json" ]; then
-#     # ensure the data dir exists
-#     mkdir -p $CODER_DOTFILES/.chezmoidata
-#     ln -s $HOME/.vworkstation/metadata.json $CODER_DOTFILES/.chezmoidata/vworkstation.json
-# fi
+# next link any metadata json so that chezmoi can read it
+if [ -f "$HOME/.config/deepgram/metadata.json" ] && [ ! -f "$CODER_DOTFILES/.chezmoidata/coder-instance.json" ]; then
+    # ensure the data dir exists
+    mkdir -p $CODER_DOTFILES/.chezmoidata
+    ln -s $HOME/.config/deepgram/metadata.json $CODER_DOTFILES/.chezmoidata/coder-instance.json
+fi
 
-# Set all env vars
+# Set all env vars jic
 export XDG_CONFIG_HOME=$HOME/.config
 export XDG_DATA_HOME=$HOME/.local/share
 export XDG_STATE_HOME=$HOME/.local/state
